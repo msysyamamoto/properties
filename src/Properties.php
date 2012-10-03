84c2016e93dc745c99862bbc7aac64df9e3595e4
ymmtmsys <m2yamamoto@gmail.com>
2012-10-03 21:28:57 +0900

    full model change

--- src/Properties.php
+++ src/Properties.php
@@ -7,11 +7,7 @@ class Properties
 
     public function __get($name)
     {
-        if ($this->prop_reader === null) {
-            $this->reflect();
-        }
-
-        if (isset($this->prop_reader[$name])) {
+        if ($this->isReadableProperty($name) === true) {
             return $this->$name;
         }
 
@@ -25,61 +21,51 @@ class Properties
 
     public function __set($name, $value)
     {
-        if ($this->prop_writer === null) {
-            $this->reflect();
-        }
-
-        if (isset($this->prop_writer[$name])) {
+        if ($this->isWritableProperty($name) === true) {
             $this->$name = $value;
-            return; 
+        } else {
+            list($trace) = debug_backtrace();
+            trigger_error(
+                'Undefined property via __set(): ' . $name .
+                ' in ' . $trace['file'] . ' on line ' . $trace['line'],
+                E_USER_ERROR
+            );
         }
-
-        list($trace) = debug_backtrace();
-        trigger_error(
-            'Undefined property via __set(): ' . $name .
-            ' in ' . $trace['file'] . ' on line ' . $trace['line'],
-            E_USER_ERROR
-        );
     }
 
-    public function reflect()
+    private function isReadableProperty($name)
     {
-        $this->prop_reader = array(); 
-        $this->prop_writer = array(); 
-
-        $ref = new ReflectionClass($this);
-
-        $filter = ReflectionProperty::IS_PROTECTED | ReflectionProperty::IS_PRIVATE;
-        $props = $ref->getProperties($filter);
-
-        foreach ($props as $prop) {
-            $comments = $prop->getDocComment();
-            if (empty($comments)) {
-                continue;
-            }
-
-            $lines = preg_split('/(?:\r\n|\r|\n)/', $comments, 0, PREG_SPLIT_NO_EMPTY);
-            $lines = array_map('trim', $lines);
-            foreach ($lines as $line) {
-                if (preg_match('/@(reader|writer|accessor)\z/', $line, $m) === 0) {
-                    continue;
-                }
-
-                $name = $prop->getName();
-                switch ($m[1]) {
-                    case 'reader':
-                        $this->prop_reader[$name] = true;
-                        break; 
-                    case 'writer':
-                        $this->prop_writer[$name] = true;
-                        break; 
-                    case 'accessor':
-                        $this->prop_reader[$name] = true;
-                        $this->prop_writer[$name] = true;
-                        break; 
-                }
-                break;
-            }
+        if (property_exists($this, $name) === false) {
+            return false;
         }
+
+        if (isset($this->prop_reader[$name]) === false) {
+            $comment  = $this->getPropertyComment($name);
+            $readable = preg_match('/@(?:accessor|reader)\b/', $comment) === 1;
+            $this->prop_reader[$name] = $readable;
+        }
+
+        return $this->prop_reader[$name];
+    }
+
+    private function isWritableProperty($name)
+    {
+        if (property_exists($this, $name) === false) {
+            return false;
+        }
+
+        if (isset($this->prop_writer[$name]) === false) {
+            $comment  = $this->getPropertyComment($name);
+            $writable = preg_match('/@(?:accessor|writer)\b/', $comment) === 1;
+            $this->prop_writer[$name] = $writable;
+        }
+
+        return $this->prop_writer[$name];
+    }
+
+    private function getPropertyComment($name)
+    {
+        $prop = new ReflectionProperty($this, $name);
+        return $prop->getDocComment();
     }
 }
