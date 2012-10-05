ac227fd611ec136ec84ec1fe3af58e4beff6dbb5
ymmtmsys <m2yamamoto@gmail.com>
2012-10-05 23:18:04 +0900

    add method header

--- src/Ymmtmsys/Properties/Properties.php
+++ src/Ymmtmsys/Properties/Properties.php
@@ -12,12 +12,27 @@ namespace Ymmtmsys\Properties;
 
 abstract class Properties
 {
+    /**
+     * @var Array
+     */
     private $__prop_reader = array();
 
+    /**
+     * @var Array
+     */
     private $__prop_writer = array();
 
+    /**
+     * @var Array
+     */
     private $__props = array();
 
+    /**
+     * utilized for reading data from inaccessible properties.
+     *
+     * @param  string $name  The name of the property
+     * @return mixed The value of the property
+     */
     public function __get($name)
     {
         if ($this->_isReadableProperty($name) === true) {
@@ -32,6 +47,12 @@ abstract class Properties
         );
     }
 
+    /**
+     * run when writing data to inaccessible properties.
+     *
+     * @param string $name  The name of the property
+     * @param mixed  $value The value of the property
+     */
     public function __set($name, $value)
     {
         if ($this->_isWritableProperty($name) === true) {
@@ -46,6 +67,10 @@ abstract class Properties
         }
     }
 
+    /**
+     * @param  string $name The name of the property
+     * @return bool
+     */
     private function _isReadableProperty($name)
     {
         if (isset($this->__prop_reader[$name]) === false) {
@@ -57,6 +82,10 @@ abstract class Properties
         return $this->__prop_reader[$name];
     }
 
+    /**
+     * @param  string $name The name of the property
+     * @return bool
+     */
     private function _isWritableProperty($name)
     {
         if (isset($this->__prop_writer[$name]) === false) {
@@ -68,6 +97,10 @@ abstract class Properties
         return $this->__prop_writer[$name];
     }
 
+    /**
+     * @param  string $name The name of the property
+     * @return mixed  Returns string on success, or FALSE on error.
+     */
     private function _getPropertyComment($name)
     {
         $prop = $this->_getProperty($name);
@@ -77,6 +110,10 @@ abstract class Properties
         return $prop->getDocComment();
     }
 
+    /**
+     * @param  string $name The name of the property
+     * @return mixed The value of the property
+     */
     private function _getPropertyValue($name)
     {
         $prop = $this->_getProperty($name);
@@ -86,6 +123,10 @@ abstract class Properties
         return $value;
     }
 
+    /**
+     * @param string $name  The name of the property
+     * @param mixed  $value The value of the property
+     */
     private function _setPropertyValue($name, $value)
     {
         $prop = $this->_getProperty($name);
@@ -94,6 +135,10 @@ abstract class Properties
         $prop->setAccessible(false);
     }
 
+    /**
+     * @param  string $name  The name of the property
+     * @return mixed The instance of the ReflectionProperty, or False
+     */
     private function _getProperty($name)
     {
         if (isset($this->__props[$name]) === false) {
@@ -104,6 +149,11 @@ abstract class Properties
         return $this->__props[$name];
     }
 
+    /**
+     * @param  string $klass     The name of the class to reflect
+     * @param  string $prop_name The name of the property
+     * @return mixed The instance of the ReflectionProperty, or False
+     */
     private static function _getPropertyRecursive($klass, $prop_name)
     {
         try {
